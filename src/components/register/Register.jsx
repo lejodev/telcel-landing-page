2d330b7c42946d958c73ec1c3d57361815c0c425
lejodev <alejandro.rv97@gmail.com>
2022-05-10 18:18:47 -0500

    register partially finished (pending second title)

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -12,14 +12,34 @@ const Register = () => {
       <div className="main-info">
         <div className="section-video">
           <div className="video-capture"> </div>
+          <div className="player"></div>
         </div>
         <div className="invitation">
-          <div className="title">
-            <span className="invitation-text">COMPRA, CONCURSA Y GANA</span>
-          </div>
+          <div className="invitation-title"></div>
+          <span className="invitation-text">COMPRA, CONCURSA Y GANA</span>
           <Button variant="outline-info"> CONOCE COMO</Button>
         </div>
-        <section className="participate">PARTICIPA</section>
+        <section className="participate">
+          <div className="participation-details-title">
+            <span>CONOCE COMO </span>
+            <span>PARTICIPAR</span>
+          </div>
+          <div className="text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+              perferendis, sint optio, porro expedita deserunt eligendi ipsum
+              quaerat magni, cupiditate nemo id quia corporis alias dolor atque
+              voluptates culpa reprehenderit?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+              sequi fugiat ad, commodi eius libero recusandae odio quaerat,
+              cumque modi veniam blanditiis iste facilis dolorum laboriosam
+              itaque necessitatibus quos dolorem.
+            </p>
+          </div>
+          <Button variant="info">Inscríbete</Button>
+        </section>
       </div>
     </div>
   );
