faaa6417a29fde45c439190a209b5f45dfe3f0d8
lejodev <alejandro.rv97@gmail.com>
2022-05-09 21:56:50 -0500

    file structure defined

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <ul className="menu-nav">
+        <input type="checkbox" name="display-menu" id="display-menu" />
+      </ul>
+      <div className="logo">LOGO HERE</div>
+      <button>Comienza Ahora</button>
+    </nav>
+  );
+};
+
+export default Nav;
